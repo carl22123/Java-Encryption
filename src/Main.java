939c771a41ab8db917ab2a82f8d075b53e4d0bcb
carl22123 <carlcain2205@gmail.com>
2025-05-26 21:18:04 +0800

    UI is done for now

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,15 @@ import java.util.ArrayList;
 
 import static java.lang.reflect.Array.set;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main <T>  extends JFrame
+public class Main <T> extends JFrame
 {
-
-    // this is a challenged for my self if i can make it shorter
-
-    private Frame frame;
-
     private JTextField insertKey;
-    private JTextArea text;
+    private JTextArea text, output ;
     private JButton generateKey, encrypt, decrypt;
-    private JLabel Key, WText;
 
     // array for there prespective components
-    private T[] TComponents = (T[]) new Object[2];
+    private T[] TComponents = (T[]) new Object[3]; // text fields and text areas
     private JButton[] BComponent = new JButton[3];
-    private JLabel[] LComponent = new JLabel[2];
 
     public static void main(String[] args)
     {
@@ -32,50 +23,28 @@ public class Main <T>  extends JFrame
     {
         setLayout(null);
         setResizable(false);
+        getContentPane().setBackground(Color.darkGray);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Encryption");
 
         setBounds(100,100,700,500);
 
         // text fields and text areas
-        TComponents[0] = (T) textfield(insertKey, 10,10,250,50);
-        TComponents[1] = (T) textArea(text, 10,105,250,270);
+        TComponents[0] = (T) textfield(insertKey, 10,10,380,50);
+        TComponents[1] = (T) textArea(text, 10,105,380,270);
+        TComponents[2] = (T) textArea(output, 10, 10, 230, 420);
 
         // Buttons
-        BComponent[0] = Button(generateKey, "Generate Key", 10,64,250,35,1);
-        BComponent[1] = Button(encrypt, "Encrypt",10, 380,120,50,2);
-        BComponent[2] = Button(decrypt, "Decrypt", 140, 380,120,50,3);
+        BComponent[0] = Button(generateKey, "Generate Key", 10,64,380,35,1);
+        BComponent[1] = Button(encrypt, "Encrypt",10, 380,185,50,2);
+        BComponent[2] = Button(decrypt, "Decrypt", 205, 380,185,50,3);
 
         // panels
-        add(panel(new JPanel(), Color.gray, 10,10, 400, 440, (T[]) TComponents, (T[]) BComponent, 1));
-        add(panel(new JPanel(), Color.BLUE, 420,10, 250, 440, null, null, 2));
+        add(panel(new JPanel(), Color.gray, 10,10, 400, 440, (T[]) TComponents, (T[]) BComponent, null,1));
+        add(panel(new JPanel(), Color.gray, 420,10, 250, 440, null, null, TComponents[2],2));
 
         setLocationRelativeTo(null); // I put it here so Frame will be in the Center
         setVisible(true);
-    }
-
-    // T for generics
-    // this will how every panel is made
-    private JPanel panel(JPanel panel, Color color, int x, int y, int width, int height, T[] Component, T[] SecondComponent, int specificPanel)
-    {
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBackground(color);
-        panel.setBounds(x, y, width, height);
-
-        // add to what panel
-        if(specificPanel == 1 ) // this is for panel 1
-        {
-            // text field
-            for(int i = 0; i < Component.length; i++)
-                panel.add((java.awt.Component) Component[i]);
-
-            // Button
-            for(int j = 0; j < SecondComponent.length; j++)
-                panel.add((java.awt.Component) SecondComponent[j]);
-        }
-
-        return panel;
     }
 
     // note: i cannot combine textfield and textArea because different object/class
@@ -102,5 +71,33 @@ public class Main <T>  extends JFrame
         button.setText(buttonText);
 
         return button;
+    }
+
+    // T for generics
+    // this will how every panel is made
+    private JPanel panel(JPanel panel, Color color, int x, int y, int width, int height, T[] Component, T[] SecondComponent, T specificComponent, int specificPanel)
+    {
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(color);
+        panel.setBounds(x, y, width, height);
+
+        // add to what panel
+        if(specificPanel == 1) // this is for panel 1
+        {
+            // text field
+            for(int i = 0; i < Component.length; i++)
+                panel.add((java.awt.Component) Component[i]);
+
+            // Button
+            for(int j = 0; j < SecondComponent.length; j++)
+                panel.add((java.awt.Component) SecondComponent[j]);
+        }
+
+        if(specificPanel == 2) // this is for panel 2
+        {
+            panel.add((java.awt.Component) specificComponent);
+        }
+        return panel;
     }
 }
