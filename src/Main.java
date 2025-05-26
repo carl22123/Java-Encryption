f249a1cf75123f5a6fae12fdb017b312434941cd
carl22123 <carlcain2205@gmail.com>
2025-05-26 14:17:07 +0800

    change a text field to a text area and added a method

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,15 @@ public class Main <T>  extends JFrame
 
     private Frame frame;
 
-    private JTextField insertKey, text;
+    private JTextField insertKey;
+    private JTextArea text;
     private JButton generateKey, encrypt, decrypt;
     private JLabel Key, WText;
 
-    private JTextField[] TComponents = {insertKey, text}; // arraylist for components like textfield
-    private JButton[] BComponent = {generateKey, encrypt, decrypt};
-    private JLabel[] LComponent = {Key, WText};
+    // array for there prespective components
+    private T[] TComponents = (T[]) new Object[2];
+    private JButton[] BComponent = new JButton[3];
+    private JLabel[] LComponent = new JLabel[2];
 
     public static void main(String[] args)
     {
@@ -31,24 +33,30 @@ public class Main <T>  extends JFrame
         setLayout(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Encryption");
 
         setBounds(100,100,700,500);
 
-        // text field
-        TComponents[0] =  textfield(insertKey, 10,70,250,50,2);
-        TComponents[1] =  textfield(text, 10,130,250,50,3);
+        // text fields and text areas
+        TComponents[0] = (T) textfield(insertKey, 10,10,250,50);
+        TComponents[1] =(T) textArea(text, 10,100,250,250);
 
-        add(panel(new JPanel(), Color.green, 10,10, 400, 440, (T[]) TComponents, null, 1));
+        // Buttons
+        BComponent[0] = Button(generateKey, "Generate Key", 10,10,50,50,1);
+        BComponent[1] = Button(encrypt, "Encrypt",10, 10,50,50,2);
+        BComponent[2] = Button(decrypt, "Decrypt", 10, 10,50,50,3);
+
+        // panels
+        add(panel(new JPanel(), Color.green, 10,10, 400, 440, (T[]) TComponents, (T[]) BComponent, 1));
         add(panel(new JPanel(), Color.BLUE, 420,10, 250, 440, null, null, 2));
 
         setLocationRelativeTo(null); // I put it here so Frame will be in the Center
         setVisible(true);
     }
 
-    //10, 10, 400,440
     // T for generics
     // this will how every panel is made
-    private JPanel panel(JPanel panel, Color color, int x, int y, int width, int height, T[] Component, T[] SComponent, int specificPanel)
+    private JPanel panel(JPanel panel, Color color, int x, int y, int width, int height, T[] Component, T[] SecondComponent, int specificPanel)
     {
         panel = new JPanel();
         panel.setLayout(null);
@@ -58,15 +66,20 @@ public class Main <T>  extends JFrame
         // add to what panel
         if(specificPanel == 1 ) // this is for panel 1
         {
-            panel.add((java.awt.Component) Component[0]);
-            panel.add((java.awt.Component) Component[1]);
+            // text field
+            for(int i = 0; i < Component.length; i++)
+                panel.add((java.awt.Component) Component[i]);
+
+            // Button
+            for(int j = 0; j < Component.length; j++)
+                panel.add((java.awt.Component) SecondComponent[j]);
         }
 
         return panel;
     }
 
     // this is how textfield is made
-    private JTextField textfield(JTextField field, int x, int y, int width, int height, int specificTextField)
+    private JTextField textfield(JTextField field, int x, int y, int width, int height)
     {
         field = new JTextField();
         field.setBounds(x, y, width, height);
@@ -74,9 +87,19 @@ public class Main <T>  extends JFrame
         return field;
     }
 
-    private JButton Button(JButton button, int x, int y, int width, int height, int specificTextField)
+    private JTextArea textArea(JTextArea field, int x, int y, int width, int height)
+    {
+        field = new JTextArea();
+        field.setBounds(x, y, width, height);
+
+        return field;
+    }
+
+    private JButton Button(JButton button, String buttonText, int x, int y, int width, int height, int specificTextField)
     {
         button = new JButton();
+        button.setBounds(x,y,width,height);
+        button.setText(buttonText);
 
         return button;
     }
